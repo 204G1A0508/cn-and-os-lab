7e76421d4cd2d48c41d553b9ff8361620b5611f8
204G1A0508 <94513619+204G1A0508@users.noreply.github.com>
2022-08-30 11:28:01 +0530

    Create T Allisha  Created Sample.c

---
+++ T Allisha  Created Sample.c
@@ -0,0 +1,10 @@
+/* Name Of the Candidate: T. Allisha
+   Roll No: 204G1A0508
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 30-08-2022
+   Date of Execution: 30-08-2022
+*/
+#include <stdio.h>
+    int main(){
+	printf("Hello World\n");
+ }
