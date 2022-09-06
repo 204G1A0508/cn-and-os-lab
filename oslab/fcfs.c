f8dbaa9788e37f9f7d44ac97b01a7707f18dfc32
204G1A0508 <94513619+204G1A0508@users.noreply.github.com>
2022-09-06 12:27:51 +0530

    committed fcfs

--- oslab/fcfs.c
+++ oslab/fcfs.c
@@ -1,8 +1,8 @@
 /* Name Of the Candidate: T. Allisha
    Roll No: 204G1A0508
    Title of the Experiment: CPU Scheduling Using FCFS Algorithm
-   Date of Creation: 30-08-2022
-   Date of Execution: 30-08-2022
+   Date of Creation: 06-09-2022
+   Date of Execution: 06-09-2022
 */
 #include<stdio.h>
 #include<conio.h>
